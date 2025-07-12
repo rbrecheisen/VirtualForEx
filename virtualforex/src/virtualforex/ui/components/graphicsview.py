4410c440c708447c43a91cf154cc0803d4658958
Ralph Brecheisen <ralph.brecheisen@gmail.com>
2025-07-12 09:31:58 +0200

    Saving work

---
+++ virtualforex/src/virtualforex/ui/components/graphicsview.py
@@ -0,0 +1,117 @@
+from PySide6.QtWidgets import (
+    QGraphicsView, QGraphicsScene, QGraphicsEllipseItem,
+)
+from PySide6.QtGui import (
+    QWheelEvent,
+    QMouseEvent,
+    QPainter,
+    QColor,
+)
+from PySide6.QtCore import (
+    Qt,
+    QRectF,
+    QPointF,
+)
+
+
+class GraphicsView(QGraphicsView):
+    def __init__(self):
+        super(GraphicsView, self).__init__()
+        self._scene = None
+        self._scene_item = None
+        self._panning = None
+        self._last_mouse_position = None
+        self.init_view()
+
+    # GET/SET
+
+    def scene_item(self):
+        if not self._scene_item:
+            self._scene_item = QGraphicsEllipseItem(QRectF(50, 50, 100, 100))
+            self._scene_item.setBrush(QColor(200, 200, 200))
+        return self._scene_item
+
+    def panning(self):
+        if not self._panning:
+            self._panning = False
+        return self._panning
+    
+    def set_panning(self, panning):
+        self._panning = panning
+    
+    def last_mouse_position(self):
+        if not self._last_mouse_position:
+            self._last_mouse_position = QPointF()
+        return self._last_mouse_position
+    
+    def set_last_mouse_position(self, last_mouse_position):
+        self._last_mouse_position = last_mouse_position
+
+    # INITIALIZATION
+
+    def init_view(self):
+        self.setRenderHint(QPainter.RenderHint.Antialiasing)
+        self.setDragMode(QGraphicsView.NoDrag)
+        self.setTransformationAnchor(QGraphicsView.AnchorUnderMouse)
+        self.setResizeAnchor(QGraphicsView.AnchorViewCenter)
+        self.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        self.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        scene = QGraphicsScene(self)
+        scene.setSceneRect(-10000, -10000, 20000, 20000)
+        scene.addItem(self.scene_item())
+        self.setScene(scene)
+
+    # EVENT HANDLERS
+
+    def wheelEvent(self, event):
+        zoom_factor = 1.15
+        old_pos = self.mapToScene(event.position().toPoint())
+        if event.angleDelta().y() > 0:
+            self.scale(zoom_factor, zoom_factor)
+        else:
+            self.scale(1/zoom_factor, 1/zoom_factor)
+        new_pos = self.mapToScene(event.position().toPoint())
+        delta = new_pos - old_pos
+        self.translate(delta.x(), delta.y())
+
+    def mousePressEvent(self, event):
+        if event.button() == Qt.LeftButton and event.modifiers() == Qt.ShiftModifier:
+            self.set_panning(True)
+            self.set_last_mouse_position(event.position())
+            self.setCursor(Qt.ClosedHandCursor)
+        else:
+            return super().mousePressEvent(event)
+        
+    def mouseMoveEvent(self, event):
+        if self.panning():
+            delta = event.position() - self.last_mouse_position()
+            self.set_last_mouse_position(event.position())
+            self.translate(delta.x(), delta.y())
+        else:
+            return super().mouseMoveEvent(event)
+        
+    def mouseReleaseEvent(self, event):
+        if event.button() == Qt.LeftButton and self.panning():
+            self.set_panning(False)
+            self.setCursor(Qt.ArrowCursor)
+        else:
+            return super().mouseReleaseEvent(event)
+        
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_R:
+            self.resetTransform()
+            self.centerOn(self.scene_item())
+        else:
+            super().keyPressEvent(event)
+        
+    def drawBackground(self, painter, rect):
+        painter.save()
+        painter.setPen(QColor(230, 230, 230))
+        grid_size = 100
+        left = int(rect.left()) - (int(rect.left()) % grid_size)
+        top = int(rect.top()) - (int(rect.top()) % grid_size)
+        for x in range(left, int(rect.right()), grid_size):
+            painter.drawLine(x, rect.top(), x, rect.bottom())
+        for y in range(top, int(rect.bottom()), grid_size):
+            painter.drawLine(rect.left(), y, rect.right(), y)
+        painter.restore()
--- virtualforex/src/virtualforex/ui/components/splashscreen.py
+++ virtualforex/src/virtualforex/ui/components/splashscreen.py
@@ -10,9 +10,7 @@ from PySide6.QtWidgets import (
 from PySide6.QtCore import Qt
 from PySide6.QtGui import QPixmap
 
-from virtualforex.ui.constants import Constants as constants
-
-from virtualforex.ui.mainwindow import MainWindow
+from virtualforex.ui.constants import Constants
 from virtualforex.ui.utils import resource_path, set_opacity
 
 
@@ -43,42 +41,42 @@ class SplashScreen(QWidget):
     def background_pixmap(self):
         if not self._background_pixmap:
             self._background_pixmap = QPixmap(resource_path(os.path.join(
-                constants.VIRTUALFOREX_RESOURCES_IMAGES_DIR, 
-                constants.VIRTUALFOREX_SPLASH_SCREEN_BACKGROUND_IMAGE,
+                Constants.VIRTUALFOREX_RESOURCES_IMAGES_DIR, 
+                Constants.VIRTUALFOREX_SPLASH_SCREEN_BACKGROUND_IMAGE,
             ))).scaled(self.size(), Qt.IgnoreAspectRatio, Qt.SmoothTransformation)
-            self._background_pixmap = set_opacity(self._background_pixmap, constants.VIRTUALFOREX_SPLASH_SCREEN_BACKGROUND_IMAGE_OPACITY)
+            self._background_pixmap = set_opacity(self._background_pixmap, Constants.VIRTUALFOREX_SPLASH_SCREEN_BACKGROUND_IMAGE_OPACITY)
         return self._background_pixmap
     
     def title_label(self):
         if not self._title_label:
-            self._title_label = QLabel(constants.VIRTUALFOREX_SPLASH_SCREEN_TITLE)
-            self._title_label.setStyleSheet(constants.VIRTUALFOREX_SPLASH_SCREEN_TITLE_STYLE_SHEET)
+            self._title_label = QLabel(Constants.VIRTUALFOREX_SPLASH_SCREEN_TITLE)
+            self._title_label.setStyleSheet(Constants.VIRTUALFOREX_SPLASH_SCREEN_TITLE_STYLE_SHEET)
             self._title_label.setAlignment(Qt.AlignCenter)
         return self._title_label
     
     def sub_text_label(self):
         if not self._sub_text_label:
-            self._sub_text_label = QLabel(constants.VIRTUALFOREX_SPLASH_SCREEN_SUB_TEXT)
-            self._sub_text_label.setStyleSheet(constants.VIRTUALFOREX_SPLASH_SCREEN_SUB_TEXT_STYLE_SHEET)
+            self._sub_text_label = QLabel(Constants.VIRTUALFOREX_SPLASH_SCREEN_SUB_TEXT)
+            self._sub_text_label.setStyleSheet(Constants.VIRTUALFOREX_SPLASH_SCREEN_SUB_TEXT_STYLE_SHEET)
             self._sub_text_label.setAlignment(Qt.AlignCenter)
         return self._sub_text_label
     
     def start_app_button(self):
         if not self._start_app_button:
-            self._start_app_button = QPushButton(constants.VIRTUALFOREX_SPLASH_SCREEN_START_APP_BUTTON_TEXT)
+            self._start_app_button = QPushButton(Constants.VIRTUALFOREX_SPLASH_SCREEN_START_APP_BUTTON_TEXT)
             self._start_app_button.clicked.connect(self.handle_start_app_button)
         return self._start_app_button
     
     def donate_button(self):
         if not self._donate_button:
-            self._donate_button = QPushButton(constants.VIRTUALFOREX_DONATE_BUTTON_TEXT)
-            self._donate_button.setStyleSheet(constants.VIRTUALFOREX_DONATE_BUTTON_STYLESHEET)
+            self._donate_button = QPushButton(Constants.VIRTUALFOREX_DONATE_BUTTON_TEXT)
+            self._donate_button.setStyleSheet(Constants.VIRTUALFOREX_DONATE_BUTTON_STYLESHEET)
             self._donate_button.clicked.connect(self.handle_donate_button)
         return self._donate_button
     
     def close_button(self):
         if not self._close_button:
-            self._close_button = QPushButton(constants.VIRTUALFOREX_SPLASH_SCREEN_CLOSE_BUTTON_TEXT)
+            self._close_button = QPushButton(Constants.VIRTUALFOREX_SPLASH_SCREEN_CLOSE_BUTTON_TEXT)
             self._close_button.clicked.connect(self.handle_close_button)
         return self._close_button
     
@@ -92,7 +90,7 @@ class SplashScreen(QWidget):
         layout.addWidget(self.donate_button())
         layout.addWidget(self.close_button())
         self.setLayout(layout)
-        self.setFixedSize(constants.VIRTUALFOREX_SPLASH_SCREEN_WINDOW_W, constants.VIRTUALFOREX_SPLASH_SCREEN_WINDOW_H)
+        self.setFixedSize(Constants.VIRTUALFOREX_SPLASH_SCREEN_WINDOW_W, Constants.VIRTUALFOREX_SPLASH_SCREEN_WINDOW_H)
         self.setWindowFlags(Qt.FramelessWindowHint)
     
     # EVENT HANDLERS
@@ -102,13 +100,13 @@ class SplashScreen(QWidget):
         self.main_window().show()
 
     def handle_donate_button(self):
-        webbrowser.open(constants.VIRTUALFOREX_DONATE_URL)
+        webbrowser.open(Constants.VIRTUALFOREX_DONATE_URL)
 
     def handle_close_button(self):
         self.close()
 
-    def resizeEvent(self, event):
-        super().resizeEvent(event)
-        scaled = self.background_pixmap().scaled(self.size(), Qt.KeepAspectRatioByExpanding, Qt.SmoothTransformation)
-        self.background_label().setPixmap(scaled)
-        self.background_label().setGeometry(0, 0, self.width(), self.height())
+    # def resizeEvent(self, event):
+    #     super().resizeEvent(event)
+    #     scaled = self.background_pixmap().scaled(self.size(), Qt.KeepAspectRatioByExpanding, Qt.SmoothTransformation)
+    #     self.background_label().setPixmap(scaled)
+    #     self.background_label().setGeometry(0, 0, self.width(), self.height())
