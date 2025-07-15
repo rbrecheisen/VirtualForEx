9c83428946ffa2df1c4fddbddf5be4587555abd4
Brecheisen <r.brecheisen@maastrichtuniversity.nl>
2025-07-15 14:01:36 +0200

    Created trade history class

--- virtualforex/src/virtualforex/core/tradehistory.py
+++ virtualforex/src/virtualforex/core/tradehistory.py
@@ -1,2 +1,19 @@
+import os
+import json
+
+
 class TradeHistory:
-    pass
+    def __init__(self):
+        self._trades = self.load_trades_from_json()
+
+    def trades(self):
+        return self._trades
+
+    def load_trades_from_json(self):
+        return []
+    
+    def save_trades_to_json(self):
+        pass
+
+    def add_trade(self, trade):
+        self.trades().append(trade)
